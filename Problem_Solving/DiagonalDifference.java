cfddf75f2fb087ca01d49e4f762331c6a1b70283
500060722 <500060722@stu.upes.ac.in>
2019-10-11 23:14:54 +0530

    Corrected Typo

--- Problem_Solving/DiagonalDifference.java
+++ Problem_Solving/DiagonalDifference.java
@@ -10,7 +10,7 @@ import java.util.stream.*;
 import static java.util.stream.Collectors.joining;
 import static java.util.stream.Collectors.toList;
 
-class Result {
+class DiagonalDifference {
 
 
     public static int diagonalDifference(List<List<Integer>> arr) {
